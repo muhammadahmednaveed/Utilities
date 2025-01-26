e50b3f6d9c8352719a4cdcc6090b3f9cc8de103c
Ahmed Naveed <ahmed.naveed2000@gmail.com>
2025-01-26 23:00:33 +0500

    Tail Recursion

---
+++ TailRecursion(TCO)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace TailRecursion_TCO_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var a = Factorial(50000);
+            var b = FactorialTailRecursive(9630,1);
+            //Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+        static BigInteger FactorialTailRecursive(int n, BigInteger product)
+        {
+            if (n < 2)
+                return product;
+            return FactorialTailRecursive(n - 1, n * product);
+        }
+
+        static BigInteger Factorial(int n)
+        {
+            if (n < 2)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
